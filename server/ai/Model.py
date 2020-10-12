44d9035817063ac600c3d6992f17367105dbf6ad
2019aliu <aliu338@gatech.edu>
2020-10-12 17:57:18 -0400

    Model and Boilerplate

--- server/ai/Model.py
+++ server/ai/Model.py
@@ -1,9 +1,153 @@
+import InformationSet
+import numpy as np
+
 class Model:
     
-    def __init__(history, hand):
-        self.history = history
-        self.hand = hand
-        self.pr1 = 1
-        self.pr2 = 1
-
+    def __init__(self, history, num_cards):
+        self.history = ""                
+        self.num_actions = 2
+        self.pr_1 = 1
+        self.pr_2 = 1
+        self.pr_c = 1
         
+        self.card_1 = -1
+        self.card_2 = -1
+
+        self.info_map = {}
+        self.iterations = 10000
+        self.expected_game_value = 0
+
+        self.num_cards = num_cards
+        self.n_possibilities = self.num_cards * (self.num_cards - 1)
+
+        for i in range(self.iterations):
+            # Add CFR to expected game value
+            self.expected_game_value += self.cfr(self.info_map, self.history, self.card_1, self.card_2, self.pr_1, self.pr_2, self.pr_c)
+            # expected_game_value += [insert cfr call here]            
+            for i_set in info_map:
+                i_set.next_strategy()
+        self.expected_game_value /= iterations
+
+        display_results(self.expected_game_value, self.info_map)
+
+
+    # recursive method -- check for base case
+    def cfr(self, i_map, history, card_1, card_2, pr_1, pr_2, pr_c):
+        # if we've reached a chance node
+        if (self.is_chance_node()):
+            self.chance_util(i_map)
+
+        # if we've reached a terminal node (game is finished)
+        if (self.is_terminal_node()):
+            self.terminal_util()
+
+        # Calculate the length of history to find whose turn it is
+        history_len = len(history)        
+        
+        # Check whose turn it is (0 for Player 1, and 1 for Player 2)
+        player_1_turn = n % 2 == 0
+
+        # Get the information set
+        info_set = get_info_set(i_map, card1 if not player_1_turn else card2, history)
+
+        # Get the appropriate strategy from the information set
+        info_set_strategy = info_set.strategy        
+
+        # Update the reach probabilities accordingly
+        if player_1_turn:
+            info_set.reach_pr += pr_1
+        else:
+            info_set.reach_pr += pr_2
+        
+        # Counterfactual utility per action
+        action_utils = np.zeros(self.num_actions)
+
+        # Run through all the possibile actions (in Kuhn Poker, it is just check and bet)
+        for i, action in enumerate(["c", "b"]):
+            next_history = history + action
+            if player_1_turn:
+                action_utils[i] = -1 * cfr(self, i_map, next_history, card_1, card_2, pr_1 * strategy[i], pr_2, pr_c)
+            else:
+                action_utils[i] = -1 * cfr(self, i_map, next_history, card_1, card_2, pr_1, pr_2 * strategy[i], pr_c)
+
+        # Utility of information sets
+        util = sum(action_utils * strategy)
+        regrets = action_utils - util
+        if player_1_turn:
+            info_set.regret_sum += pr_2 * pr_c * regrets
+        else:
+            info_set.regret_sum += pr_1 * pr_c * regrets
+        
+        return util
+
+
+    def get_info_set(i_map, card, history):
+        key = card + " " + history
+        if key not in i_map:            
+            info_set = InformationSet(key, 3)
+            i_map[key] = info_set
+            return info_set
+        return i_map[key]
+
+    def is_chance_node(self):
+        return self.history == ""
+    
+    def chance_util(self, i_map):
+        expected_value = 0
+        for i in range(self.num_cards):
+            for j in range(self.num_cards):
+                if i != j:
+                    expected_value += self.cfr(i_map, "rr", i, j, 1, 1, 1 / self.n_possibilities)  # TODO: fix this method call
+        return expected_value / n_possibilities
+    
+    def is_terminal_node(history):
+        terminal_nodes = ["rrcbc", "rrcc", "rrcbb", "rrbc", "rrbb"]
+        return history in terminal_nodes
+    
+    def terminal_util(history, card_1, card_2):
+        # Initialize length of history to find whose turn it is
+        n = len(history)
+
+        this_card = card_1 if (n % 2 == 0) else card_2
+        opp_card = card_2 if (n % 2 == 1) else card_1
+
+        # If last player folds, we win
+        if (history == "rrcbc" or history == "rrbc"):
+            return 1
+
+        # If both players check, greater card wins
+        if (history == "rrcc"):
+            if (this_card > opp_card):
+                return 1
+            return -1
+        
+        # If both players bet, greater card wins with double reward/loss
+        if (history == "rrcbb" or history == "rrbb"):
+            if (this_card > opp_card):
+                return 2
+            return -2
+    
+    def card_str(card):
+        if card == 0:
+            return "J"
+        elif card == 1:
+            return "Q"
+        return "K"
+    
+    def display_results(ev, i_map):
+        print("Player 1 Expected Value: {}".format(ev))
+        print("Player 2 Expected Value: {}".format(-1 * ev))
+
+        print()
+        print("Player 1 Strategies: ")
+        sorted_items = sorted(i_map.items(), key=lambda x: x[0])
+        for _, v in filter(lambda x: len(x[0]) % 2 == 0, sorted_items):
+            print(v)
+        print()
+        print("Player 2 Strategies: ")
+        for _, v in filter(lambda x : len(x[0]) % 2 == 1, sorted_items):
+            print(v)
+
+if __name__ == "__main__":
+    # call functionality here
+    test_model = Model("", 3)
--- server/logic/PokerGame.py
+++ server/logic/PokerGame.py
@@ -3,45 +3,135 @@ from poker import Suit, Rank, Card
 
 class PokerGame:
 
-    def __init__(self, numPlayers):
-        # Jack, Queen, King    
-        self.jack = Rank('J')
-        self.queen = Rank('Q')
-        self.king = Rank('K')
-        deck = [jack, queen, king]       
+    def __init__(self, num_players, num_cards, input_deck, small_blind, big_blind, player_amounts):
+        '''
+        Basic info
+        '''
+        # Number of players + cards
+        if (num_players * num_cards > len(self.deck)):
+            raise ValueError("The combination of the number of players and the number of cards for each player does not match")
+        self.num_players = num_players
+        self.num_cards = num_cards
+        # Small, big blind
+        self.small_blind = small_blind
+        self.big_blind = big_blind
+        # Player starting amounts
+        self.player_amounts = player_amounts
+        self.deck = [Card(card) for card in input_deck]
 
-        # Distribute the cards to each player
-        random.shuffle(deck)
-        self.deck = deck
-        self.hands = deck[:numPlayers]
-
+        '''
+        Fields to keep track of the playing field
+        '''
         # Initialize pot of the game
         self.pot = 0
-
-        # History of the Game (random chance event, check action, bet action)
-        self.history = "rr"
-
         # Cards on the table
         self.cardsOnTable = []
+        # Index of card in deck
+        self.deck_index = 0
 
-        # Small, big blind
-        self.smallBlind = ""
-        self.bigBlind = ""        
-
-    def get_info_set(i_map, card, history):
-        key = card + " " + history
-        if key not in i_map:            
-            info_set = InformationSet(key, 3)
-            i_map[key] = info_set
-            return info_set
-        return i_map[key]
-
+        '''
+        Stats
+        '''
+        # History of the Game (random chance event, check action, bet action)
+        self.history = ""
     
+    def resetRound(self):
+        self.pot = 0
+        self.cardsOnTable = []
+        self.deck_index = 0
+    
+    def dealCards(self):
+        # Distribute the cards to each player
+        random.shuffle(self.deck)
+        self.hands = [self.deck[self.num_cards * i: self.num_cards * (i + 1)] for i in range(self.num_players)]
+        self.deck_index = self.num_players * self.num_cards
+    
+    def dealCardsOnTable(self, num_deal, num_burn):
+        self.deck_index += num_burn
+        for i in range(num_deal):
+            if (deck_index > len(self.deck) - 1):
+                raise ValueError("Ran out of cards")
+            self.cardsOnTable.append(self.deck[deck_index])
+            deck_index += 1
+
+    def flop(self):
+        self.dealCardsOnTable(3, 1)
+    
+    def turn(self):
+        self.dealCardsOnTable(1, 1)
+    
+    def river(self):
+        self.dealCardsOnTable(1, 1)
+
     def makeAction(self, player, action, amount):
         pass
 
-    def isTerminal(self, history):
+    def isRoundEnd(self, history):
         pass
 
-    # def getBestAction(self, player):
-    #     pass
+    def isGameEnd(self):
+        return sum([1 if self.player_amounts[playerIndex] > 0 else 0 for playerIndex in self.player_amounts]) <= 1
+    
+    '''
+    Getters and setters
+    '''
+    def get_num_players(self):
+        return self.num_players
+    
+    def get_num_cards(self):
+        return self.num_cards
+    
+    def get_small_blind(self):
+        return self.small_blind
+    
+    def set_small_blind(self, small_blind):
+        self.small_blind = small_blind
+    
+    def get_big_blind(self):
+        return self.big_blind
+    
+    def set_big_blind(self, big_blind):
+        self.big_blind = big_blind
+    
+    def get_player_amounts(self):
+        return self.player_amounts
+    
+    def set_player_amounts(self, player_amounts):
+        self.player_amounts = player_amounts
+    
+    def get_deck(self):
+        return self.deck
+    
+    def set_deck(self, deck):
+        self.deck = deck
+
+    def get_pot(self):
+        return self.pot
+    
+    def set_pot(self, pot):
+        self.pot = pot
+
+    def get_cardsOnTable(self):
+        return self.cardsOnTable
+    
+    def set_cardsOnTable(self, cardsOnTable):
+        self.cardsOnTable = cardsOnTable
+
+    def get_history(self):
+        return self.history
+    
+    def set_history(self, history):
+        self.history = history
+    
+        
+if __name__ == "__main__":
+    # Kuhn Poker setup
+    deck = ["Js", "Qs", "Ks"]
+    game = PokerGame(
+        num_players=2
+        num_cards=1
+        input_deck=deck
+        small_blind=1
+        big_blind=1
+        player_amounts={"Alice": 2, "Bob": 2}
+    )
